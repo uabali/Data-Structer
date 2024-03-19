8af20587d1e14ccbb400eb516dedf00b51dee0c8
Umut ABALI <ua63aba@outlook.com>
2024-03-19 23:10:21 +0300

    Initial commit

---
+++ Big-O.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void findNemo1(char **array) {
+    if (strcmp(array[0], "nemo") == 0) {
+        printf("Found NEMO!\n");
+    }
+}
+
+int main() {
+    char *nemo[] = {"nemo"};
+    findNemo1(nemo);
+    return 0;
+}
