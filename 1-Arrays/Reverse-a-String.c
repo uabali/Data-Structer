89084a75813b96f046b41da9aa3a578635e56936
Umut ABALI <ua63aba@outlook.com>
2024-03-28 14:45:30 +0300

    new commit

---
+++ 1-Arrays/Reverse-a-String.c
@@ -0,0 +1,25 @@
+//
+// Created by Umut Abalı on 28.03.2024.
+//
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char array1[] = "Merhaba Umut";
+    int array_size = strlen(array1);
+    char array2[array_size];
+
+
+
+    // array1 dizisinin tersini array2 dizisine kopyalama
+    for (int i = 0; i < array_size; i++) {
+        array2[i] = array1[array_size - 1 - i];
+    }
+    array2[array_size] = '\0'; // array2 dizisine null karakter ekleme
+
+    // Ters çevrilmiş diziyi yazdırma
+    printf("%s\n", array2);
+
+    return 0;
+}
